dee3e8f4dd4c83ae4a2bd226c98066efdb49e56a
Matthew Daly <matthewd673@outlook.com>
2018-01-20 03:03:56 +0000

    Rearrange render code + a whole lot more

--- game.js
+++ game.js
@@ -1,14 +1,44 @@
 var mySprite;
+var boxes = [];
 
 window.onload = function()
 {
     //document.write("loaded");
-    createCanvas(800, 600);
+    createCanvas(800, 600, document.getElementById("container"));
     mySprite = new Sprite("sprite.png", 32, 32);
     play();
 }
 
 onUpdate = function()
 {
-    mySprite.move(mySprite.x + 1, mySprite.y + 1);
+    mySprite.x++;
+    mySprite.y++;
+    
+    for(var i = 0; i < boxes.length; i++)
+    {
+        boxes[i].x++;
+        boxes[i].y++;
+        draw(boxes[i]);
+    }
+    
+    draw(mySprite);
+    
+}
+
+function removeSprite()
+{
+    hide(mySprite);
+}
+
+function countSprites()
+{
+    console.log("Sprites: " + (boxes.length));
+}
+
+onMouseMove = function(x, y)
+{
+    //var newSprite = new Sprite("sprite.png", 32, 32, x, y);
+    //boxes.push(newSprite);
+    var newShape = new Shape("rectangle", 32, 32, true, x, y);
+    boxes.push(newShape);
 }
--- spring.js
+++ spring.js
@@ -1,11 +1,12 @@
 var canvas;
 var context;
+var canvasBounding;
 var resourcesReady;
 
-var sprites = [];
+var renderList = [];
 
 //SETUP
-function createCanvas(width, height, parent = null)
+function createCanvas(width, height, parent = document)
 {
     var canvasCode = "<canvas id='spring-surface'></canvas>";
     
@@ -23,19 +24,21 @@ function createCanvas(width, height, parent = null)
     }
     else
     {
-        var parent = document.getElementById(parent);
         parent.appendChild(newCanvas);
     }
     
     canvas = newCanvas;
     context = canvas.getContext("2d");
-    
+    canvasBounding = canvas.getBoundingClientRect();
+    setupInput();
 }
 
 function setCanvas(canvasId)
 {
     canvas = document.getElementById(canvasId);
     context = canvas.getContext("2d");
+    canvasBounding = canvas.getBoundingClientRect();
+    setupInput();
 }
 //END SETUP
 
@@ -57,27 +60,64 @@ class Sprite
         this.height = height;
         this.x = x;
         this.y = y;
-        sprites.push(this);
     }
     
-    hide()
-    {
-        this.render = false;
-    }
-    
-    show()
+}
+
+class Shape
+{
+    constructor(type, width, height, filled = false, x = 0, y = 0)
     {
         this.render = true;
-    }
-    
-    move(x, y)
-    {
+        this.ready = false;
+        
+        this.type = type.toLowerCase();
+        this.width = width;
+        this.height = height;
+        this.filled = filled;
         this.x = x;
         this.y = y;
     }
     
 }
 //END RENDERING CLASSES
+
+//RENDERING HELPERS
+function hide(renderObject)
+{
+    renderObject.render = false;
+}
+
+function show(renderObject)
+{
+    renderObject.render = true;
+}
+//END RENDERING HELPERS
+
+//INPUT SETUP
+function setupInput()
+{
+    canvas.addEventListener("click", canvasClicked);
+    canvas.addEventListener("mousemove", canvasMouseMoved);
+}
+//END INPUT SETUP
+
+//INPUT STUFF
+function canvasClicked(event)
+{
+    onClick(event.clientX - canvasBounding.left, event.clientY - canvasBounding.top);
+}
+
+function onClick(x, y) { }
+
+function canvasMouseMoved(event)
+{
+    onMouseMove(event.clientX - canvasBounding.left, event.clientY - canvasBounding.top);
+}
+
+function onMouseMove(x, y) { }
+
+//END INPUT
 
 //UPDATE FUNCTIONS
 function update()
@@ -87,12 +127,12 @@ function update()
     if(!resourcesReady)
     {
         var readyCount = 0;
-        for(var i = 0; i < sprites.length; i++)
+        for(var i = 0; i < renderList.length; i++)
         {
-            if(sprites[i].ready)
+            if(renderList[i].ready)
                 readyCount++;
         }
-        if(readyCount == sprites.length - 1)
+        if(readyCount == renderList.length - 1)
             resourcesReady = true;
     }
     render();
@@ -103,17 +143,51 @@ function onUpdate() {} //just here to prevent errors
 function render()
 {
     context.clearRect(0, 0, canvas.width, canvas.height);
-    for(var i = 0; i < sprites.length; i++)
+    for(var i = 0; i < renderList.length; i++)
     {
-        var currentSprite = sprites[i];
-        if(currentSprite.render)
+        var current = renderList[i];
+        var type = current.constructor.name;
+        
+        if(current.render && inFrame(current))
         {
-            //render it
-            context.drawImage(currentSprite.image, currentSprite.x, currentSprite.y);
+            if(type == "Sprite")
+                context.drawImage(current.image, current.x, current.y);
+            if(type == "Shape")
+            {
+                if(current.type == "rectangle")
+                    if(current.filled)
+                        context.fillRect(current.x, current.y, current.width, current.height);
+                    else
+                        context.strokeRect(current.x, current.y, current.width, current.height);
+            }
         }
     }
+    
+    renderList.splice(0, renderList.length);
+    
 }
 //END UPDATE FUNCTIONS
+
+function draw(renderObject, x = null, y = null)
+{
+    if(x != null && y != null)
+    {
+        renderObject.x = x;
+        renderObject.y = y;
+    }
+    renderList.push(renderObject);
+}
+
+function inFrame(renderObject)
+{
+    var type = renderObject.constructor.name;
+
+    if(renderObject.x > canvasBounding.width || renderObject.y > canvasBounding.height || (renderObject.x + renderObject.width) < 0 || (renderObject.y + renderObject.height) < 0) //incomplete
+        return false;
+    else
+        return true;
+    
+}
 
 function play(fps = 60)
 {
